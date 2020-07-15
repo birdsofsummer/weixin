1716bc0e709a4618ecd5edc6ad4b3b6882bb8c3f
小舟从此逝 <kopor_oo@qq.com>
2020-07-15 16:43:47 +0800

    ..

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -56,9 +56,9 @@ const jsApiList=[
         'translateVoice',
         'startRecord',
         'stopRecord',
-        'onVoiceRecordEnd',
+        'onVoiceRecordEnd', //
         'playVoice',
-        'onVoicePlayEnd',
+        'onVoicePlayEnd',  //
         'pauseVoice',
         'stopVoice',
         'uploadVoice',
@@ -73,13 +73,52 @@ const jsApiList=[
         'hideOptionMenu',
         'showOptionMenu',
         'closeWindow',
-        'scanQRCode',
+        'scanQRCode', //
         'chooseWXPay',
         'openProductSpecificView',
         'addCard',
         'chooseCard',
         'openCard',
 ]
+
+api_name= {
+	"checkJsApi": "是否支持指定JS",
+	"onMenuShareTimeline": "分享到朋友圈",
+	"onMenuShareAppMessage": "分享给朋友",
+	"onMenuShareQQ": "分享到QQ",
+	"onMenuShareWeibo": "分享到腾讯微博",
+	"onMenuShareQZone": "分享到QZone",
+	"chooseImage": "拍照或从手机相册中选图",
+	"previewImage": "预览图片",
+	"uploadImage": "上传图片",
+	"downloadImage": "下载图片",
+	"startRecord": "开始录音",
+	"stopRecord": "停止录音",
+	"playVoice": "播放语音",
+	"pauseVoice": "暂停播放",
+	"stopVoice": "停止播放",
+	"uploadVoice": "上传语音",
+	"downloadVoice": "下载语音",
+	"translateVoice": "识别音频并返回识别结果",
+	"getNetworkType": "获取网络状态",
+	"openLocation": "使用微信内置地图查看位置",
+	"getLocation": "获取地理位置",
+	"hideOptionMenu": "隐藏右上角菜单",
+	"showOptionMenu": "显示右上角菜单",
+	"closeWindow": "关闭当前网页窗口",
+	"hideMenuItems": "批量隐藏功能按钮",
+	"showMenuItems": "批量显示功能按钮",
+	"hideAllNonBaseMenuItem": "隐藏所有非基础按钮",
+	"showAllNonBaseMenuItem": "显示所有功能按钮",
+	"scanQRCode": "调起微信扫一扫",
+	"openProductSpecificView": "跳转微信商品页",
+	"addCard": "批量添加卡券",
+	"chooseCard": "调起适用于门店的卡券列表并获取用户选择列表",
+	"openCard": "查看微信卡包中的卡券",
+	"chooseWXPay": "发起一个微信支付请求",
+}
+
+
 
 vcomponents=[
 	"ActionSheet",
@@ -174,9 +213,11 @@ vcomponents=[
 get=(u,d={})=>axios.get(u,d).then(x=>x.data)
 
 wx.ready(function(e){
+    vant.Toast("ready");
     console.log('rrrrrrr',e)
 });
 wx.error(function(res){
+    vant.Toast("error");
     console.log('111111',res)
 });
 
@@ -220,6 +261,18 @@ init=async()=>{
 Vue.use(vant.Lazyload);
 vcomponents.forEach(x=>Vue.use(vant[x]))
 
+
+
+  // 3 智能接口
+  var voice = {
+    localId: '',
+    serverId: ''
+  };
+
+
+
+
+
 events={
     openLocation:{
         latitude: 22.545538,
@@ -232,7 +285,6 @@ events={
       jsApiList: ['chooseImage'],
       success: function(res) {
          vant.Toast(res);
-      // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
       }
     },
     updateAppMessageShareData:{
@@ -252,18 +304,128 @@ events={
              vant.Toast("share success");
         },
     },
-    onMenuShareTimeline:{
-        title: 'aa', // 分享标题
-        link: 'cc', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: 'dd', // 分享图标
-        success: function () {
-             vant.Toast("share success");
+    onMenuShareAppMessage:{
+              title: '互联网之子',
+              desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+              link: 'http://movie.douban.com/subject/25785114/',
+              imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+              trigger: function (res) {
+                    alert('用户点击发送给朋友');
+              },
+              success: function (res) {
+                alert('已分享');
+              },
+              cancel: function (res) {
+                alert('已取消');
+              },
+              fail: function (res) {
+                alert(JSON.stringify(res));
+              }
         },
+        onMenuShareTimeline:{
+              title: '互联网之子',
+              link: 'http://movie.douban.com/subject/25785114/',
+              imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+              trigger: function (res) {
+                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                alert('用户点击分享到朋友圈');
+              },
+              success: function (res) {
+                alert('已分享');
+              },
+              cancel: function (res) {
+                alert('已取消');
+              },
+              fail: function (res) {
+                alert(JSON.stringify(res));
+              },
+        },
+        onMenuShareQQ:{
+              title: '互联网之子',
+              desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+              link: 'http://movie.douban.com/subject/25785114/',
+              imgUrl: 'http://img3.douban.com/view/movie_poster_cover/spst/public/p2166127561.jpg',
+              trigger: function (res) {
+                alert('用户点击分享到QQ');
+              },
+              complete: function (res) {
+                alert(JSON.stringify(res));
+              },
+              success: function (res) {
+                alert('已分享');
+              },
+              cancel: function (res) {
+                alert('已取消');
+              },
+              fail: function (res) {
+                alert(JSON.stringify(res));
+              }
+        },
+        onMenuShareWeibo:{
+              title: '互联网之子',
+              desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+              link: 'http://movie.douban.com/subject/25785114/',
+              imgUrl: 'http://img3.douban.com/view/movie_poster_cover/spst/public/p2166127561.jpg',
+              trigger: function (res) {
+                alert('用户点击分享到微博');
+              },
+              complete: function (res) {
+                alert(JSON.stringify(res));
+              },
+              success: function (res) {
+                alert('已分享');
+              },
+              cancel: function (res) {
+                alert('已取消');
+              },
+              fail: function (res) {
+                alert(JSON.stringify(res));
+              }
 
-    },
-
-    //...
+        },
+        onMenuShareQZone:{
+              title: '互联网之子',
+              desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+              link: 'http://movie.douban.com/subject/25785114/',
+              imgUrl: 'http://img3.douban.com/view/movie_poster_cover/spst/public/p2166127561.jpg',
+              trigger: function (res) {
+                alert('用户点击分享到QZone');
+              },
+              complete: function (res) {
+                alert(JSON.stringify(res));
+              },
+              success: function (res) {
+                alert('已分享');
+              },
+              cancel: function (res) {
+                alert('已取消');
+              },
+              fail: function (res) {
+                alert(JSON.stringify(res));
+              }
+        },
 }
+
+
+
+
+
+translateVoice=function () {
+        if (voice.localId == '') {
+          alert('请先使用 startRecord 接口录制一段声音');
+          return;
+        }
+        wx.translateVoice({
+          localId: voice.localId,
+          complete: function (res) {
+            if (res.hasOwnProperty('translateResult')) {
+              alert('识别结果：' + res.translateResult);
+            } else {
+              alert('无法识别');
+            }
+          }
+        });
+     }
 
 
 
@@ -271,6 +433,8 @@ app= new Vue({
     el: '#app',
   //  template: `<van-button>按钮</van-button>`,
     created(){
+
+
     },
     methods:{
      say(x){
@@ -303,6 +467,7 @@ app= new Vue({
             ],
             events,
             jsApiList,
+            api_name,
             buttons:[
                 "default",
                 "primary",
